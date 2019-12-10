d81ecdd083bda3c624ae098d3dd3529ffeec0496
LorenzoSalaKK <55480959+LorenzoSalaKK@users.noreply.github.com>
2019-12-10 20:35:04 +0100

    v1

--- sketch.js
+++ sketch.js
@@ -1,11 +1,43 @@
+var porygon, portex;
+
 function preload(){
-  // put preload code here
+  porygon = loadModel("./assets/porygon.obj", true)
+  portex = loadImage('./assets/porygon4.png')
+  substitute = loadModel("./assets/Substitute.obj", true)
+  subtex = loadImage("./assets/glitch.jpg")
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth,windowHeight, WEBGL)
+  angleMode(DEGREES)
+
 }
 
 function draw() {
-  // put drawing code here
+  background('black')
+  var locx = mouseX - windowWidth / 2;
+  var locy = mouseY - windowHeight / 2;
+  // background('black')
+
+
+  ambientLight(15, 15, 15)
+  pointLight(200, 255, 255, locx, locy, 1000)
+  noStroke()
+
+  push()
+  rotateY(180 + frameCount * 0.5)
+  rotateX(120 + frameCount * 0.65)
+  rotateZ(- 15 + frameCount * 0.33)
+  scale(2)
+  texture(portex)
+  model(porygon)
+  pop()
+  rotateZ(frameCount)
+
+  translate(0,windowHeight/2.5,0)
+  rotateY(frameCount*3)
+  scale(0.5)
+
+  texture(subtex)
+  model(substitute)
 }
